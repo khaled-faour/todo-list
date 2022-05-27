0bbaa6f8685fe6eeb7bef11f6ff5887cd6f5e6dd
Khaled Faour <khaledfaour2@gmail.com>
2022-05-27 11:22:34 +0300

    Added New Todo Item Save Functionality

--- js/todo.js
+++ js/todo.js
@@ -2,28 +2,37 @@
 let addModal = document.getElementById("add-modal");
 let closeAddBtn = document.getElementById('add-close');
 let addBtn = document.getElementById('add-btn');
+let saveTodo = document.getElementById('save-new-todo');
 
+// Open Add New Todo Modal
 addBtn.onclick=()=>{
     addModal.style.display = 'block'
 }
 
+// Close Add New Todo Modal
 closeAddBtn.onclick = () => {
     addModal.style.display = 'none'
 }
 
-const todoItems = localStorage.getItem('todo-items') || [];
+// Get Todo Items From Local Storage
+const todoItems = JSON.parse(localStorage.getItem('todo-items')) || [];
 
+// Save New Todo Item
+saveTodo.onclick=()=>{
+    const title = document.getElementById('title-input').value
+    const point = document.getElementById('point-input').value
+    const description = document.getElementById('description-input').value
 
-
-// Todo Item Class
-class Todo {
-	constructor(title, description, point){
-  	this.id = Date.now()+Math.floor((Math.random()*100000))
-    this.title = title
-    this.description = description
-    this.point = point
-    this.isDone = false
-    this.createdAt = new Date(Date.now())
-  }
-  
+    console.log({title, description, point})
+    let newItem = {
+        id : Date.now()+Math.floor((Math.random()*100000)),
+        title : title,
+        description : description,
+        point : point,
+        is_done : false,
+        created_at : new Date(Date.now())
+    }
+    todoItems.push(newItem)
+    localStorage.setItem('todo-items', JSON.stringify(todoItems))
 }
+
