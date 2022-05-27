f3f7220b3cb7b393da95760647040952dab3b852
Khaled Faour <khaledfaour2@gmail.com>
2022-05-27 10:47:06 +0300

    Added New Todo Modal Form

--- js/todo.js
+++ js/todo.js
@@ -1,1 +1,12 @@
 // TODO LIST JAVASCRIPT FILE
+let addModal = document.getElementById("add-modal");
+let closeAddBtn = document.getElementById('add-close');
+let addBtn = document.getElementById('add-btn');
+
+addBtn.onclick=()=>{
+    addModal.style.display = 'block'
+}
+
+closeAddBtn.onclick = () => {
+    addModal.style.display = 'none'
+}
